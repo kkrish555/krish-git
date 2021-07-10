b8d251b1d91f041451035668f19b0b4b2c0e65e9
kkrish555 <87226069+kkrish555@users.noreply.github.com>
2021-07-10 16:20:11 +0530

    Add files via upload

---
+++ one-oneclass.py
@@ -0,0 +1,18 @@
+class Emp:
+    def __init__(self,id,name,salary):
+        self.id=id
+        self.name=name
+        self.salary=salary
+
+    def display(self):
+        print("\n Name {} of employee,\n employee ID is {}, \n Salary of employee is {}".format(self.name,self.id,self.salary))
+
+class Myclass:
+    @staticmethod
+    def mymethod(e):
+       e.salary+=1000;
+       e.display()
+
+e=Emp("i572077","gopi",25000)
+e.display()
+Myclass.mymethod(e)
